ebbe47ab0a04299d2a0be76383fba2f1317a65a6
Josh <surettej@bu.edu>
2018-10-07 19:28:15 -0400

    integrating projects api call into ui

--- src/components/work/Work.js
+++ src/components/work/Work.js
@@ -1,11 +1,55 @@
-import React from 'react'
+import React, { Component } from 'react'
+import axios from 'axios'
 
-const Work = () => {
-	return (
-		<div>
-			<h1>TODO</h1>
-		</div>
-	);
+class Work extends Component {
+	constructor(props) {
+		super(props);
+
+		this.initialState = {
+			totalProjects: 0,
+			projects: []
+		};
+
+		this.state = this.initialState;
+	}
+
+	updateProjectState() {
+		axios.get('/projects/search/text')
+		.then(response => {
+			if(response.data) {
+				this.modifyState(response.data.totalHits, response.data.hits)
+				return;
+			}
+			throw new Error('no data');
+		})
+		.catch(error => {
+			console.log(error);
+		})
+	}
+
+	modifyState = (totalHits, results) => {
+		this.setState({
+			totalProjects: totalHits,
+			projects: results
+		});
+	}
+
+	componentDidMount() {
+		this.updateProjectState()	
+	}
+
+	render() {
+		return (
+			<div>
+				<h1>Total: {this.state.totalProjects}</h1>
+				<ul>
+					{this.state.projects.map(project => 
+						<li key={project.id}>{project.sourceAsMap.title}</li>
+					)}
+				</ul>
+			</div>
+		)
+	}
 }
 
 export default Work;
