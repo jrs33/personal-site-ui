2e433cc6919407f31377edde4904e8d13d1cb54f
Josh <surettej@bu.edu>
2018-10-11 00:02:39 -0400

    adding styling to home page render

--- src/components/home_page/Home.js
+++ src/components/home_page/Home.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import profile from '../../profile.png'
 import { SocialIcon } from 'react-social-icons';
+import './Home.css'
 
 const Home = () => {
 	return (
 		<div>
 			<img src={ profile } alt="profile_picture" />
-			<h1> Hi! I'm Josh Surette </h1>
-			<SocialIcon url="https://twitter.com/joshsurette3" color="red" />
-			<SocialIcon url="https://joshsuretteblog.com/" type="wordpress" color="red" />
-			<SocialIcon url="https://github.com/jrs33" color="red" />
+			<h1 className="centered"> Josh Surette </h1>
+			<h6 className="centered"> Software Engineer @ Drift </h6>
+			<hr />
+			<div className="centered">
+				<SocialIcon url="https://twitter.com/joshsurette3" color="red" />
+				<SocialIcon url="https://joshsuretteblog.com/" type="wordpress" color="red" />
+				<SocialIcon url="https://github.com/jrs33" color="red" />
+			</div>
 		</div>
 	);
 }
