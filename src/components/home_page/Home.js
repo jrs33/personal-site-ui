f9dcdba0ec1cd0c58dd4c4233df929e7ca53735a
Josh <surettej@bu.edu>
2018-10-14 20:37:50 -0400

    finalizing home page design for release 1

--- src/components/home_page/Home.js
+++ src/components/home_page/Home.js
@@ -1,19 +1,24 @@
 import React from 'react'
-import profile from '../../profile.png'
+import icon from '../../favicon.ico'
+import elasticsearch from '../../elasticsearch.png';
 import { SocialIcon } from 'react-social-icons';
 import './Home.css'
 
 const Home = () => {
 	return (
 		<div>
-			<img src={ profile } alt="profile_picture" />
-			<h1 className="centered"> Josh Surette </h1>
-			<h6 className="centered"> Software Engineer @ Drift </h6>
-			<hr />
+			<img src={ icon } alt="profile_picture" />
 			<div className="centered">
 				<SocialIcon url="https://twitter.com/joshsurette3" color="red" />
 				<SocialIcon url="https://joshsuretteblog.com/" type="wordpress" color="red" />
 				<SocialIcon url="https://github.com/jrs33" color="red" />
+			</div>
+			<hr />
+			<div className="thanks">
+				<p> Special thanks to: </p>
+				<a href="https://www.elastic.co/">
+					<img className="icon" src={ elasticsearch } alt="elasticsearch" />
+				</a>
 			</div>
 		</div>
 	);
